6ce7c4878c401c383785e6cd32fd76eb047b8d3f
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-22 20:18:50 +0300

    Update cartoon_collections.py

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, name in enumerate(dwarves, start=1):
+        print(f"{i}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    capitalized_calls = [call.capitalize() + "!" for call in planeteer_calls]
+    return capitalized_calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(strings):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for string in strings:
+        if string in cheese_types:
+            return string
+    return None
+
